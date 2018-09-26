7be6a36d9ace62dd05de66b8d183703a6a6ffc2f
Tezika <zfk565614653@outlook.com>
2018-09-26 11:55:47 -0600

    setup the files structure

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -2,10 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Test : MonoBehaviour {
+public class Item : MonoBehaviour {
 
 	// Use this for initialization
-    void Start () {
+	void Start () {
 		
 	}
 	
