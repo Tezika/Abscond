c92f4e409ecefaf6057f9ad13c5d3232401bee10
Tezika <zfk565614653@outlook.com>
2018-09-26 17:52:40 -0600

    add the asset file to handle collectibles instance

---
+++ Assets/Scripts/Assets/Collectibles.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Abs.Inventory.Assets
+{
+    [CreateAssetMenu(fileName = "Collectibles",menuName = "Abs/Inventory/Collectibles")]
+    public class Collectibles : ScriptableObject
+    {
+        public List<Collectible> collectibles = new List<Collectible>();
+    }
+}
+
--- Assets/Scripts/InventoryHandler.cs
+++ Assets/Scripts/InventoryHandler.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Abs.Inventory.Assets;
+using Abs.Utils;
 using UnityEngine;
 
 namespace Abs.Inventory
@@ -7,6 +9,15 @@ namespace Abs.Inventory
     public class InventoryHandler : MonoBehaviour
     {
 
+        public Collectibles collectibleAsset;
+
+        private void Awake()
+        {
+            if (this.collectibleAsset == null)
+            {
+                Debugger.Exception("Please assign the collectible asset to inventory");
+            }
+        }
         // Use this for initialization
         void Start()
         {
