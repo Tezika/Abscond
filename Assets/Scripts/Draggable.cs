c2daf4189f0ed0b51e1cfc139c87e661f32bd19a
Srija <u1209446@utah.edu>
2018-09-29 15:36:14 -0600

    Added in some generic scripts

---
+++ Assets/Scripts/Draggable.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Draggable : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    void Update()
+    {
+
+    }
+
+    void OnMouseDrag()
+    {
+        float x = Input.mousePosition.x;
+        float y = Input.mousePosition.y;
+        float z = gameObject.transform.position.z;
+
+        Vector3 point = Camera.main.ScreenToWorldPoint(new Vector3(x, y, z));
+        gameObject.transform.position = point;
+
+        Debug.Log("x: " + point.x + "   y: " + point.y + "   z: " + point.z);
+    }
+}
---
+++ Assets/Scripts/TriggerObject.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerObject : MonoBehaviour {
+
+    /*
+     This script can be added onto any object on which you can drag another object.
+     Make sure it has a Collider2D component with "IsTrigger" set.
+         */
+
+    public GameObject correctObject;
+    private Callback[] callback;
+    
+    // Use this for initialization
+	void Start () {
+        callback = GetComponents<Callback>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject != correctObject)
+            return;
+        foreach (Callback x in callback)
+            x.Invoke();
+    }
+}
