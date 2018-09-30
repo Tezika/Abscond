a1e5e70749b235a7dcdf73d550a36567c244489e
Tezika <tezikazhou@gmail.com>
2018-09-30 11:23:27 -0600

    add the callback to instantiate a object

--- Assets/Scripts/CallBacks/Callback.cs
+++ Assets/Scripts/CallBacks/Callback.cs
@@ -1,9 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Abs.Item;
 using UnityEngine;
 
 namespace Abs.Callbacks
 {
+    [RequireComponent(typeof(TriggerObject))]
     public class Callback : MonoBehaviour
     {
     
---
+++ Assets/Scripts/CallBacks/InstantiateCallback.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Abs.Utils;
+
+namespace Abs.Callbacks
+{
+    public class InstantiateCallback : Callback
+    {
+        public GameObject prefab;
+
+        public Transform instantiateTrans;
+
+        private void Awake()
+        {
+            if (this.prefab == null)
+            {
+                Debugger.Exception("Please assign the prefab to this prefab field");
+            }
+
+            if (this.instantiateTrans == null)
+            {
+                Debugger.Exception("Please assign the transform to this field");
+            }
+        }
+
+        public override void Invoke()
+        {
+            base.Invoke();
+            Instantiate(this.prefab, this.instantiateTrans.transform.position, Quaternion.identity);
+        }
+    }
+}
+
