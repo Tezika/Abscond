17cb80c8b81e438e67de2990321be6c531f18781
Tezika <zfk565614653@outlook.com>
2018-09-30 10:15:58 -0600

    change the scene a little bit & got the collision work

--- Assets/Scripts/TriggerObject.cs
+++ Assets/Scripts/TriggerObject.cs
@@ -1,33 +1,42 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Abs.Utils;
 using UnityEngine;
 
-public class TriggerObject : MonoBehaviour {
-
-    /*
-     This script can be added onto any object on which you can drag another object.
-     Make sure it has a Collider2D component with "IsTrigger" set.
-         */
-
-    public GameObject correctObject;
-    private Callback[] callback;
-    
-    // Use this for initialization
-	void Start () {
-        callback = GetComponents<Callback>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-
-    private void OnTriggerEnter2D(Collider2D collision)
+namespace Abs.Item
+{
+    public class TriggerObject : MonoBehaviour
     {
-        if (collision.gameObject != correctObject)
-            return;
-        Debug.Log("Trigger");
-        foreach (Callback x in callback)
-            x.Invoke();
+
+        /*
+         This script can be added onto any object on which you can drag another object.
+         Make sure it has a Collider2D component with "IsTrigger" set.
+             */
+
+        public GameObject correctObject;
+        private Callback[] callbacks;
+
+        // Use this for initialization
+        void Start()
+        {
+            callbacks = GetComponents<Callback>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            Debugger.Log("Trigger the functions");
+            if (other.gameObject != correctObject)
+                return;
+            Debug.Log("Trigger");
+            foreach (Callback x in callbacks)
+                x.Invoke();
+        }
     }
+
 }
