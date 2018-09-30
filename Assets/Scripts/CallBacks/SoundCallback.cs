f39b6db01f2b774af5a2e7f702a94aa07f3d3336
Tezika <tezikazhou@gmail.com>
2018-09-30 11:41:26 -0600

    add callbacks
    For animation callback &  sound callback.

---
+++ Assets/Scripts/CallBacks/SoundCallback.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Abs.Callbacks
+{
+    public class SoundCallback : Callback
+    {
+        private void Awake()
+        {
+
+        }
+
+        public override void Invoke()
+        {
+            base.Invoke();
+        }
+    }
+}
+
