fb0052e17f0648ae14982446fe5055bd72c7ee3e
Tezika <tezikazhou@gmail.com>
2018-10-02 08:37:18 -0600

    add the test sfx

--- Assets/Scripts/CallBacks/SoundCallback.cs
+++ Assets/Scripts/CallBacks/SoundCallback.cs
@@ -4,16 +4,25 @@ using UnityEngine;
 
 namespace Abs.Callbacks
 {
+    [RequireComponent(typeof(AudioSource))]
     public class SoundCallback : Callback
     {
+        public List<AudioClip> clips = new List<AudioClip>();
+
+        private AudioSource _source;
+
         private void Awake()
         {
-
+            _source = this.GetComponent<AudioSource>();
         }
 
         public override void Invoke()
         {
             base.Invoke();
+            for (int i = 0; i < this.clips.Count; i++)
+            {
+                _source.PlayOneShot(clips[i]);
+            }
         }
     }
 }
