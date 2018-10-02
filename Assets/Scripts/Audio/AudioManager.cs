214697c73ddc008403e7a323ee6bbdc58bacd1f5
Tezika <tezikazhou@gmail.com>
2018-10-02 13:06:59 -0600

    Fix the audio playing problem

---
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+namespace Abs.Audio
+{
+    [RequireComponent(typeof(AudioSource))]
+    public class AudioManager : MonoBehaviour
+    {
+        public static AudioManager instance;
+
+        private AudioSource _source;
+
+        private void Awake()
+        {
+            AudioManager.instance = this.gameObject.GetComponent<AudioManager>();
+            _source = this.gameObject.GetComponent<AudioSource>();
+        }
+
+        public void PlayAudio(AudioClip audioClip)
+        {
+            if (_source != null)
+            {
+                _source.PlayOneShot(audioClip);
+            }
+        }
+
+        // Update is called once per frame
+        private void Update()
+        {
+
+        }
+    }
+}
+
--- Assets/Scripts/CallBacks/SoundCallback.cs
+++ Assets/Scripts/CallBacks/SoundCallback.cs
@@ -1,30 +1,24 @@
-﻿using Abs.Utils;
-using System.Collections.Generic;
+﻿using Abs.Audio;
 using UnityEngine;
 
 namespace Abs.Callbacks
 {
-    [RequireComponent(typeof(AudioSource))]
     public class SoundCallback : Callback
     {
-        public List<AudioClip> clips = new List<AudioClip>();
-
-        private AudioSource _source;
+        public AudioClip clip;
 
         private void Awake()
         {
-            _source = this.GetComponent<AudioSource>();
         }
 
         public override void Invoke()
         {
             base.Invoke();
-            for (int i = 0; i < this.clips.Count; i++)
+            if (clip != null)
             {
-                Debugger.Log("Play the sound:" + this.clips[i].name);
-
-                _source.PlayOneShot(this.clips[i]);
+                AudioManager.instance.PlayAudio(clip);
             }
+             //_source.PlayOneShot(this.clips[i]);
         }
     }
 }
